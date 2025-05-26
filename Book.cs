31a4030f2debd317ab327e6e3e83a72e26fd0999
hanantawafsha <hananjtawafsha@gmail.com>
2025-05-26 03:45:45 +0300

    upload files

---
+++ Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+   public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public bool IsAvailable { get; set; } = true;
+
+        public Book (int ID, string Title, string AuthorName)
+        {
+            this.ID = ID;
+            this.Title = Title;
+            this.AuthorName = AuthorName;
+        }
+        public void DisplayInfo()
+        {
+            // to implement it
+            string status = IsAvailable ? "Available" : "Not Available";
+
+            Console.WriteLine($"Book ID: {ID}, Title: {Title}, Author: {AuthorName}, Status: {status}");
+
+        }
+    }
+}
--- obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8cc47da51756e9a76c5ca6fb082e83d8eafb813")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
